686d021b433ef76686d814afb5849bad959629b9
enesakar <enesakar@gmail.com>
2018-11-24 20:18:27 -0800

    remove ssl config

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ var Client = require('hazelcast-client').Client;
 var ClientConfig = require('hazelcast-client').Config.ClientConfig;
 
 
-function createClientConfigWithSSLOpts() {
+function createClientConfig() {
     var cfg = new ClientConfig();
-    var sslOpts = {
-        servername: 'hazelcast.cloud',
-        rejectUnauthorized: true,
-    };
     cfg.networkConfig.cloudConfig.enabled = true;
     cfg.networkConfig.cloudConfig.discoveryToken = 'YOUR_CLUSTER_DISCOVERY_TOKEN ';
     cfg.networkConfig.redoOperation = true;
     cfg.networkConfig.connectionAttemptLimit = 10;
     cfg.groupConfig.name = 'YOUR_CLUSTER_NAME';
     cfg.groupConfig.password = 'YOUR_CLUSTER_PASSWORD';
-    cfg.networkConfig.sslOptions = sslOpts;
     return cfg;
 }
 
-var cfg = createClientConfigWithSSLOpts();
+var cfg = createClientConfig();
 
 Client.newHazelcastClient(cfg).then(function (client) {
     var map = client.getMap("testMap")
