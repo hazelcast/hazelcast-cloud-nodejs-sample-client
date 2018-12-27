6379e65841748697073c062b85744850fd96e875
Hüseyin BABAL <huseyinbabal88@gmail.com>
2018-12-27 16:18:36 +0300

    Loop added

--- client.js
+++ client.js
@@ -18,19 +18,38 @@ function createClientConfig() {
 
 var cfg = createClientConfig();
 
-Client.newHazelcastClient(cfg).then(function (client) {
-    var map = client.getMap("map")
+Client.newHazelcastClient(cfg).then(function (hazelcastClient) {
+    var client = hazelcastClient;
+    var map;
+    hazelcastClient.getMap('map').then(function (mp) {
+      map = mp;
 
-    map.put('key', 'value').then(function () {
-        return map.get('key');
-    }).then((res) => {
-        if(res === 'value')
-    {
-        console.log("Connection Successful!");
-    }
-    else {
-        throw new Error("Connection failed, check your configuration.");
-    }
-    client.shutdown();
-});
+      map.put('key', 'value').then(function () {
+          return map.get('key');
+      }).then((res) => {
+          if(res === 'value')
+          {
+              console.log("Connection Successful!");
+              console.log("Now, `map` will be filled with random entries.");
+
+              async.whilst(() => {
+                return true;
+              },(next) => {
+                var randomKey = Math.floor(Math.random() * 100000);
+                map.put('key' + randomKey, 'value' + randomKey).then(function () {
+                    map.get('key' + randomKey);
+                    if (randomKey % 10 == 0) {
+                        map.size().then((size) => console.log(`map size: ${size}`));
+                    }
+                    next();
+                });
+              },(err) => {
+                client.shutdown();
+              });
+          }
+          else {
+              throw new Error("Connection failed, check your configuration.");
+          }
+      });
+    });
 });
