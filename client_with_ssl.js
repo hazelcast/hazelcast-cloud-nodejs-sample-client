063b33d5c91577c249031703404eab7047ae1f65
Umit UNAL <umit@hazelcast.com>
2019-01-23 11:02:29 +0300

    ssl config enabled added.

--- client_with_ssl.js
+++ client_with_ssl.js
@@ -5,23 +5,30 @@ var path = require('path')
 
 function createClientConfig() {
     var fs = require('fs');
-    var cfg = new ClientConfig();
+
     var caFile = path.resolve(path.join(__dirname, 'ca.pem'));
     var certFile = path.resolve(path.join(__dirname, 'cert.pem'));
     var keyFile = path.resolve(path.join(__dirname, 'key.pem'));
-    cfg.networkConfig.sslOptions = {
+
+    var cfg = new ClientConfig();
+    
+    cfg.networkConfig.sslConfig.enabled = true;
+    cfg.networkConfig.sslConfig.sslOptions = {
         rejectUnauthorized: false,
         ca: [fs.readFileSync(caFile)],
         cert: [fs.readFileSync(certFile)],
         key: [fs.readFileSync(keyFile)],
         passphrase: 'YOUR_SSL_PASSWORD'
     };
+
     cfg.networkConfig.cloudConfig.enabled = true;
     cfg.networkConfig.cloudConfig.discoveryToken = 'YOUR_CLUSTER_DISCOVERY_TOKEN ';
     cfg.networkConfig.redoOperation = true;
     cfg.networkConfig.connectionAttemptLimit = 10;
+    
     cfg.groupConfig.name = 'YOUR_CLUSTER_NAME';
     cfg.groupConfig.password = 'YOUR_CLUSTER_PASSWORD';
+    
     cfg.properties['hazelcast.client.cloud.url'] = 'YOUR_DISCOVERY_URL';
     cfg.properties['hazelcast.client.statistics.enabled'] = true;
     cfg.properties['hazelcast.client.statistics.period.seconds'] = 1;
