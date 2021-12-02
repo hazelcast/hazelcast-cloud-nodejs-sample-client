1a5bc7e0db23b594bc964a2abd584c093f89ca1d
Sergey Dreger <sergey.dreger@hazelcast.com>
2021-12-02 16:51:02 +0200

    HZC-2040: Add 'jsonSerializationExample' to the 5.0 client sample

--- client.js
+++ client.js
@@ -83,6 +83,135 @@ async function sqlExample(hzClient) {
     process.exit(0);
 }
 
+async function jsonSerializationExample(hzClient) {
+    await createMappingForCountries(hzClient);
+
+    await populateCountriesMap(hzClient);
+
+    await selectAllCountries(hzClient);
+
+    await createMappingForCities(hzClient);
+
+    await populateCityMap(hzClient);
+
+    await selectCitiesByCountry(hzClient, "AU");
+
+    await selectCountriesAndCities(hzClient);
+
+    process.exit(0);
+}
+
+async function createMappingForCountries(hzClient) {
+    // see: https://docs.hazelcast.com/hazelcast/5.0/sql/mapping-to-maps#json-objects
+    console.log("Creating mapping for countries...");
+
+    const mappingQuery =
+        "CREATE OR REPLACE MAPPING country(" +
+        " __key VARCHAR," +
+        " isoCode VARCHAR," +
+        " country VARCHAR)" +
+        " TYPE IMap" +
+        " OPTIONS (" +
+        "     'keyFormat' = 'varchar'," +
+        "     'valueFormat' = 'json-flat'" +
+        " )";
+
+    await hzClient.getSql().execute(mappingQuery);
+    console.log("Mapping for countries has been created.");
+    console.log("--------------------");
+}
+
+async function populateCountriesMap(hzClient) {
+    // see: https://docs.hazelcast.com/hazelcast/5.0/data-structures/creating-a-map#writing-json-to-a-map
+    console.log("Populating 'countries' map with JSON values...");
+
+    const countries = await hzClient.getMap("country");
+    await countries.set("AU", {"isoCode":"AU", "country":"Australia"});
+    await countries.set("EN", {"isoCode":"EN", "country":"England"});
+    await countries.set("US", {"isoCode":"US", "country":"United States"});
+    await countries.set("CZ", {"isoCode":"US", "country":"Czech Republic"});
+    console.log("The 'countries' map has been populated.");
+    console.log("--------------------");
+}
+
+async function selectAllCountries(hzClient) {
+    const query = "SELECT c.country from country c";
+    console.log("Select all countries with sql = " + query);
+
+    const sqlResult = await hzClient.getSql().execute(query);
+    for await (const row of sqlResult) {
+        console.log("country = " + row.country);
+    }
+    console.log("--------------------");
+}
+
+async function createMappingForCities(hzClient) {
+    //see: https://docs.hazelcast.com/hazelcast/5.0/sql/mapping-to-maps#json-objects
+    console.log("Creating mapping for cities...");
+
+    const mappingQuery =
+        "CREATE OR REPLACE MAPPING city(" +
+        " __key INT," +
+        " country VARCHAR," +
+        " city VARCHAR," +
+        " population BIGINT)" +
+        " TYPE IMap" +
+        " OPTIONS (" +
+        "     'keyFormat' = 'int'," +
+        "     'valueFormat' = 'json-flat'" +
+        " )";
+
+    await hzClient.getSql().execute(mappingQuery);
+    console.log("Mapping for cities has been created.");
+    console.log("--------------------");
+}
+
+async function populateCityMap(hzClient) {
+    // see: https://docs.hazelcast.com/hazelcast/5.0/data-structures/creating-a-map#writing-json-to-a-map
+    console.log("Populating 'city' map with JSON values...");
+
+    const cities = await hzClient.getMap("city");
+    cities.set(1, {"country":"AU", "city":"Canberra", "population":354644});
+    cities.set(2, {"country":"CZ", "city":"Prague", "population":1227332});
+    cities.set(3, {"country":"EN", "city":"London", "population":8174100});
+    cities.set(4, {"country":"US", "city":"Washington, DC", "population":601723});
+    console.log("The 'city' map has been populated.");
+    console.log("--------------------");
+}
+
+async function selectCitiesByCountry(hzClient, country) {
+    const query = "SELECT city, population FROM city where country=?";
+    console.log("Select city and population with sql = " + query);
+
+    const sqlResult = await hzClient.getSql().execute(query, [country]);
+    for await (const row of sqlResult) {
+        console.log("city = %s, population = %s", row.city, row.population);
+    }
+    console.log("--------------------");
+}
+
+async function selectCountriesAndCities(hzClient) {
+    const query =
+        "SELECT c.isoCode, c.country, t.city, t.population" +
+        "  FROM country c" +
+        "  JOIN city t" +
+        "  ON c.isoCode = t.country";
+    console.log("Select country and city data in query that joins tables");
+
+    let rows = [];
+    const sqlResult = await hzClient.getSql().execute(query);
+    for await (const row of sqlResult) {
+        rows.push({
+            "iso":row.isoCode,
+            "country": row.country,
+            "city": row.city,
+            "population": parseInt(row.population)
+        });
+    }
+    console.table(rows);
+    console.log("--------------------");
+}
+
 (async () => {
     try {
         const client = await Client.newHazelcastClient(createClientConfig());
@@ -94,6 +223,8 @@ async function sqlExample(hzClient) {
         await mapExample(map);
 
         // await sqlExample(client);
+
+        // await jsonSerializationExample(client);
 
     } catch (err) {
         console.error('Error occurred:', err);
